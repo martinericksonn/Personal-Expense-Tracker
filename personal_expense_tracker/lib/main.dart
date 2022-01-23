f798a50698a1e6a257d94b439d8d2797728c5921
Martin <Lapetajemartin@gmail.com>
2022-01-24 02:06:56 +0800

    input field alpha

--- personal_expense_tracker/lib/main.dart
+++ personal_expense_tracker/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:personal_expense_tracker/transactions.dart';
 
 void main() {
@@ -30,6 +31,10 @@ class MyHomePage extends StatelessWidget {
     Transaction('02', 'Fries', 246.31, DateTime.now()),
     Transaction('03', 'Ice Cream', 152.43, DateTime.now()),
   ];
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +47,39 @@ class MyHomePage extends StatelessWidget {
               width: double.infinity,
               child: Card(
                 elevation: 1,
-                child: Text("Expense"),
+                child: Text(" "),
+              ),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: <Widget>[
+                    TextField(
+                      // onChanged: (value) => titleInput = value,
+                      controller: titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                      ),
+                    ),
+                    TextField(
+                      // onChanged: (value) => amountIntput = value,
+                      controller: amountController,
+                      decoration: InputDecoration(
+                        labelText: 'Amount',
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        print(titleController.text);
+                        print(amountController.text);
+                      },
+                      child: Text("Add Transaction"),
+                    )
+                  ],
+                ),
               ),
             ),
             Column(
@@ -82,7 +119,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            transaction.date.toString(),
+                            DateFormat.yMMMMEEEEd().format(transaction.date),
                             style: TextStyle(
                               color: Colors.grey,
                             ),
