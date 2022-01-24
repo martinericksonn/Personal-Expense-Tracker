58394dcfff8c56a84b2a28c4d5ae88605bbf3d49
Martin <Lapetajemartin@gmail.com>
2022-01-24 15:47:00 +0800

    connecting data sates

---
+++ personal_expense_tracker/lib/widgets/transaction_user.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+import 'package:personal_expense_tracker/classes/transactions.dart';
+import 'package:personal_expense_tracker/widgets/transaction_form.dart';
+import 'package:personal_expense_tracker/widgets/transaction_list.dart';
+
+class TransactionUser extends StatefulWidget {
+  TransactionUser({Key? key}) : super(key: key);
+
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final List<Transaction> _usertransactions = [
+    Transaction('01', 'Burger', 132.12, DateTime.now()),
+    Transaction('02', 'Fries', 246.31, DateTime.now()),
+    Transaction('03', 'Ice Cream', 152.43, DateTime.now()),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTransaction = Transaction(
+      DateTime.now().toString(),
+      title,
+      amount,
+      DateTime.now(),
+    );
+  }
+  // setState((){
+  // _usertransactions.add(newTransaction)
+  // }){
+
+  // }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionForm(),
+        TransactionList(_usertransactions),
+      ],
+    );
+  }
+}
