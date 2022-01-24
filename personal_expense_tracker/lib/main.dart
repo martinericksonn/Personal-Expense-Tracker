15281256f5df6178f42ac95c805b8581e19267d0
Martin <Lapetajemartin@gmail.com>
2022-01-24 14:31:55 +0800

    splitting the app into widget

--- personal_expense_tracker/lib/main.dart
+++ personal_expense_tracker/lib/main.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:personal_expense_tracker/transactions.dart';
+import 'package:personal_expense_tracker/widgets/transaction_form.dart';
+import 'package:personal_expense_tracker/widgets/transaction_list.dart';
+import 'package:personal_expense_tracker/widgets/transaction_user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,15 +27,6 @@ class MyApp extends StatelessWidget {
 
 // ignore: use_key_in_widget_constructors
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction('01', 'Burger', 132.12, DateTime.now()),
-    Transaction('02', 'Fries', 246.31, DateTime.now()),
-    Transaction('03', 'Ice Cream', 152.43, DateTime.now()),
-  ];
-
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,87 +42,7 @@ class MyHomePage extends StatelessWidget {
                 child: Text(" "),
               ),
             ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: <Widget>[
-                    TextField(
-                      // onChanged: (value) => titleInput = value,
-                      controller: titleController,
-                      decoration: InputDecoration(
-                        labelText: 'Title',
-                      ),
-                    ),
-                    TextField(
-                      // onChanged: (value) => amountIntput = value,
-                      controller: amountController,
-                      decoration: InputDecoration(
-                        labelText: 'Amount',
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        print(titleController.text);
-                        print(amountController.text);
-                      },
-                      child: Text("Add Transaction"),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Column(
-              children: transactions.map((transaction) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        // margin: EdgeInsets.symmetric(
-                        //   horizontal: 15,
-                        //   vertical: 10,
-                        // ),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        child: Text(
-                          '₱ ${transaction.amount}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transaction.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            DateFormat.yMMMMEEEEd().format(transaction.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
+            TransactionUser(),
           ],
         ));
   }
