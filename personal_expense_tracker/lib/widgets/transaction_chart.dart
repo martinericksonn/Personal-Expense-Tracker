7ad499c8067b20ad1e444d46a90073e45d2151da
Martin <Lapetajemartin@gmail.com>
2022-01-25 21:48:40 +0800

    transaction list

--- personal_expense_tracker/lib/widgets/transaction_chart.dart
+++ personal_expense_tracker/lib/widgets/transaction_chart.dart
@@ -1,9 +1,30 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, must_be_immutable
 import 'package:flutter/material.dart';
+import 'package:personal_expense_tracker/components/transactions.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({Key? key}) : super(key: key);
+  Chart(this.recentTransaction, {Key? key}) : super(key: key);
+  List<Transaction> recentTransaction;
+
+  List<Map<String, Object>> get transactionAmount {
+    return List.generate(7, (index) {
+      final dayOfWeek = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double sumAmount = 0.0;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == dayOfWeek.day &&
+            recentTransaction[i].date.month == dayOfWeek.month &&
+            recentTransaction[i].date.year == dayOfWeek.year) {
+          sumAmount += recentTransaction[i].amount;
+        }
+      }
+      return {
+        'day': 'T',
+        'amount': 199,
+      };
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
